5115c6ed5ebc010ce50f63e2dff02e1321033f3f
Jacob Gjerstad <jgjerstad@comcast.net>
2020-02-05 22:01:22 -0800

    Added data annotaions for validating clothing

--- TinyClothes/Models/Clothing.cs
+++ TinyClothes/Models/Clothing.cs
@@ -20,31 +20,40 @@ namespace TinyClothes.Models
         /// <summary>
         /// The size of the clothing (ex: small, medium, large)
         /// </summary>
+        [Required(ErrorMessage = "Size is required")]
         public string  Size { get; set; }
 
         /// <summary>
         /// The type of clothing (ex: hat, shirt, etc)
         /// </summary>
+        [Required]
         public string Type { get; set; }
 
         /// <summary>
         /// The color of the clothing
         /// </summary>
+        [Required]
         public string Color { get; set; }
 
         /// <summary>
         /// The retail price of the item
         /// </summary>
+        [Range(0.0, 300.0)]
         public double Price { get; set; }
 
         /// <summary>
         /// The display title of the clothing item
         /// </summary>
+        [Required]
+        [StringLength(35)]
+        // Sample Regex, great for validation
+        //[RegularExpression("^([A-Za-z0-9])+$")]
         public string Title { get; set; }
 
         /// <summary>
         /// Description of the clothing item
         /// </summary>
+        [StringLength(800)]
         public string Description { get; set; }
     }
 }
