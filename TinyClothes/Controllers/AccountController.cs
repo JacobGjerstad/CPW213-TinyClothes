a3ace81716075e482db9b2c49805fa761a7a5a49
Jacob <jgjerstad@comcast.net>
2020-02-20 10:41:14 -0800

    Users can now logout

--- TinyClothes/Controllers/AccountController.cs
+++ TinyClothes/Controllers/AccountController.cs
@@ -76,5 +76,11 @@ namespace TinyClothes.Controllers
 
             return View(login);
         }
+
+        public IActionResult Logout()
+        {
+            SessionHelper.DestroyUserSession(_http);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
