03debaec5d1b8b4522366047d54e046ca614d904
Jacob <jgjerstad@comcast.net>
2020-02-27 10:43:21 -0800

    User can now add multiple cart items

--- TinyClothes/Models/CartHelper.cs
+++ TinyClothes/Models/CartHelper.cs
@@ -16,7 +16,10 @@ namespace TinyClothes.Models
 
         public static void Add(Clothing c, IHttpContextAccessor http)
         {
-            string data = JsonConvert.SerializeObject(c);
+            List<Clothing> clothes = GetAllClothes(http);
+            clothes.Add(c);
+
+            string data = JsonConvert.SerializeObject(clothes);
 
             CookieOptions options = new CookieOptions()
             {
@@ -30,18 +33,25 @@ namespace TinyClothes.Models
 
         public static int GetCount(IHttpContextAccessor http)
         {
-            string data = http.HttpContext.Request.Cookies[CartCookie];
+            List<Clothing> allClothes = GetAllClothes(http);
 
-            if (string.IsNullOrEmpty(data))
-            {
-                return 0;
-            }
-            return 1;
+            return allClothes.Count();
         }
 
+        /// <summary>
+        /// Returns all clothing currently stored in the users cookie.
+        /// If no items are present an empty list is returned.
+        /// </summary>
         public static List<Clothing> GetAllClothes(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            string data = http.HttpContext.Request.Cookies[CartCookie];
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return new List<Clothing>();
+            }
+
+            return JsonConvert.DeserializeObject<List<Clothing>>(data);
         }
     }
 }
