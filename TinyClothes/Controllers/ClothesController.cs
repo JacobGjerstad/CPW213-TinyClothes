6d399e632222e451668bb1538cb27e847a3fab94
Jacob <jgjerstad@comcast.net>
2020-03-03 10:34:08 -0800

    Template search page

--- TinyClothes/Controllers/ClothesController.cs
+++ TinyClothes/Controllers/ClothesController.cs
@@ -114,5 +114,11 @@ namespace TinyClothes.Controllers
             TempData["Message"] = $"{c.Title} deleted successfully";
             return RedirectToAction(nameof(ShowAll));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(SearchCriteria search)
+        {
+            return View();
+        }
     }
 }
